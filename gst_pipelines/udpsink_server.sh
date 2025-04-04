8f5be2204254afe955dafa1513613026356e2180
unknown <leminhtu74@gmail.com>
2025-04-04 15:38:42 +0700

    fix: change pipeline

--- gst_pipelines/udpsink_server.sh
+++ gst_pipelines/udpsink_server.sh
@@ -3,10 +3,10 @@ source ../.env
 # GStreamer Server
 # Endpoint: `tcp://127.0.0.1:5000`
 ```
-GST_DEBUG=2  gst-launch-1.0 -v \
-    udpsrc port=5000 caps="application/x-rtp, media=video, encoding-name=H264, clock-rate=90000, payload=96" ! \
-    rtph264depay ! \
-    avdec_h264 ! \
-    videoconvert ! \
-    autovideosink
+GST_DEBUG=2 gst-launch-1.0 \
+    uridecodebin uri=$RTSP_1 ! \
+    x264enc tune=zerolatency ! \
+    rtph264pay config-interval=1 pt=96 ! \
+    application/x-rtp,media=video,encoding-name=H264,clock-rate=90000,payload=96 ! \
+    udpsink host=127.0.0.1 port=5000
 ```
